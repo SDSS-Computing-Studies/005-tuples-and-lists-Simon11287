3fca3fa96f5cc3ac04dfc7cd4528908d58e9be87
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-10-08 10:56:09 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,3 +14,11 @@ The smallest number is 3 and the largest number is 9
 """
 
 myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
+myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
+print(myList)
+myList.sort()
+print(myList)
+dCount = myList.count("3")
+print("The number of times the number 3 appears is :" + str(dCount))
+dCount = myList.count("9")
+print("The number of times the number 9 appears is :" + str(dCount))
